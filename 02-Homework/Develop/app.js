21357e9b85d08ed8204232b7b62712f7b7d13e85
Shaminder Singh <spsingh94.ss@gmail.com>
2020-05-04 13:01:17 -0400

    updated switch case and new employees

--- 02-Homework/Develop/app.js
+++ 02-Homework/Develop/app.js
@@ -5,7 +5,7 @@ const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 ​
-const OUTPUT_DIR = path.resolve(__dirname, "output")
+const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 ​
 const render = require("./lib/htmlRenderer");
@@ -18,104 +18,108 @@ inquirer.prompt([{
     name: "employee",
     message: "Which employee would you like to add?",
     choices: ["Engineer", "Intern", "Manager"]
-}]).then(function(response){
-switch (response.employee) {
-case "Manager": createManager();
-}
-break;
-switch (response.employee) {
-case "Intern": createIntern();
-}
-break;
-switch (response.employee) {
-case "Engineer": createEngineer();
-}
-})
-    
+}]).then(function(res){
+switch (res.employee) {
+    case "Manager": 
+        createManager();
+        break;
+    case "Intern": 
+        createIntern();
+        break;
+    case "Engineer": 
+        createEngineer();
+    };
+});
+};
 //inquirer asking which employee they want to add
 //switch statement calling next function based on user answer
 //createManager()
-}
-​function createManager(){
-    //inquirer statemetn asking all manager questions (in class)
-    inquirer.prompt([{
-        type: "input",
-        name: "name",
-        message: "What is your name?",
-    },
-    {
-        type: "input",
-        name: "id",
-        message: "What is your id?",
-    },
-    {
-        type: "input",
-        name: "email",
-        message: "What is your email?",
-    },
-    {
-        type: "input",
-        name: "office",
-        message: "What is your office number?",
-    }]).then(function(response){
-       new Manager = response.identity
-    })
-    init();
-    //create new manager using new keyword
-    //push new manager to employees arr
-    //call init func (will restart questioning)
-}
-​function createIntern(){
-    inquirer.prompt([{
-        type: "input",
-        name: "name",
-        message: "What is your name?",
-    },
-    {
-        type: "input",
-        name: "id",
-        message: "What is your id?",
-    },
-    {
-        type: "input",
-        name: "email",
-        message: "What is your email?",
-    },
-    {
-        type: "input",
-        name: "school",
-        message: "What school do you attend?",
-    }]).then(function(response){
-       new Intern = response.identity
-    })
-    init();
-}
+
+//createEngineer function
 ​function createEngineer(){
     inquirer.prompt([{
         type: "input",
         name: "name",
-        message: "What is your name?",
+        message: "What is your name?"
     },
     {
         type: "input",
         name: "id",
-        message: "What is your id?",
+        message: "What is your id?"
     },
     {
         type: "input",
         name: "email",
-        message: "What is your email?",
+        message: "What is your email?"
     },
     {
         type: "input",
         name: "github",
-        message: "What is your github?",
-    }]).then(function(response){
-       new Engineer = response.identity
+        message: "What is your github?"
+    }]).then(function(res){
+        var engineer = new Engineer(res.name, res.id, res.email, res.github)
+        employees.push(engineer);
     })
     init();
-}
+};
 
+//createIntern function
+​function createIntern(){
+    inquirer.prompt([{
+        type: "input",
+        name: "name",
+        message: "What is your name?"
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "What is your id?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your email?"
+    },
+    {
+        type: "input",
+        name: "school",
+        message: "What school do you attend?"
+    }])
+    .then(function(res){
+        var intern = new Intern(res.name, res.id, res.email, res.school);
+        employees.push(intern);
+    })
+    init();
+};
+
+//createManager function
+​function createManager(){
+    inquirer.prompt([{
+        type: "input",
+        name: "name",
+        message: "What is your name?"
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "What is your id?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your email?"
+    },
+    {
+        type: "input",
+        name: "office",
+        message: "What is your office number?"
+    }])
+    .then(function(res){
+        var manager = new Manager(res.name, res.id, res.email, res.office);
+        employees.push(manager);
+    })
+    init();
+};
 //do the same thing with other roles. (will do the same thing done on 21 to 24)
 
 // Write code to use inquirer to gather information about the development team members,
@@ -125,20 +129,20 @@ inquirer.prompt([{
     name: "continue",
     message: "Do you want to enter an employee?",
     choices: ["yes", "no"]
-}]).then(function (response){
-    if (response.continue == "yes"){
+}]).then(function (res){
+    if (res.continue == "yes"){
         addEmployee();
     } else {
        let html = render(employees);
        fs.writeFile(outputPath, html, function(error){
            if(error){
                throw error;
-           }
-           console.log ("File Successfully Written");
+            }
+            console.log ("File Successfully Written");
        })
     }
 })
-}
+};
 
 init();
 // and to create objects for each team member (using the correct classes as blueprints!)
